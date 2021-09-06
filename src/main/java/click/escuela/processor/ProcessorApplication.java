76a399335dd604c8cfe4524153ed377b31dbd3ff
umbert oscar <oscar.umbert@gmail.com>
2021-09-06 19:28:14 -0300

    eureka was configured

--- src/main/java/click/escuela/processor/ProcessorApplication.java
+++ src/main/java/click/escuela/processor/ProcessorApplication.java
@@ -2,7 +2,9 @@ package click.escuela.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProcessorApplication {
 
