2cf02b9d6bb1bf5f58d6ef52614e56f95f14ee3c
Tu Nombre <eve_aragon@hotmail.com>
2021-10-18 14:16:31 -0300

    configuracion scripts

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/processor
+sudo rm -rf /home/ec2-user/server/processor/appspec.yml
+sudo rm -rf /home/ec2-user/server/processorprocessor-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/processor/processor-service.pid
+sudo rm -rf /home/ec2-user/server/processor/logs
+sudo rm -rf /home/ec2-user/server/processor/server_stop.sh
+sudo rm -rf /home/ec2-user/server/processor/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/processor/server_prestart.sh
+sudo rm -rf /home/ec2-user/server/processor/server_clear.sh
