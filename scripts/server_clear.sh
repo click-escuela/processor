d3ec1b4032b3e9c6aef159e347d2472981f0ce50
Evelin <eve_aragon@hotmail.com>
2021-10-18 23:17:03 -0300

    subir a dev

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 sudo rm -rf /home/ec2-user/server/processor/appspec.yml
-<<<<<<< HEAD
 sudo rm -rf /home/ec2-user/server/processor/processor-0.0.1-SNAPSHOT.jar
 sudo rm -rf /home/ec2-user/server/processor/processor-service.pid
 sudo rm -rf /home/ec2-user/server/processor/logs
 sudo rm -rf /home/ec2-user/server/processor/server_stop.sh
 sudo rm -rf /home/ec2-user/server/processor/fix_privileges.sh
-=======
-sudo rm -rf /home/ec2-user/server/processorprocessor-0.0.1-SNAPSHOT.jar
-sudo rm -rf /home/ec2-user/server/processor/processor-service.pid
-sudo rm -rf /home/ec2-user/server/processor/logs
-sudo rm -rf /home/ec2-user/server/processor/server_stop.sh
-sudo rm -rf /home/ec2-user/server/processor/fix_provileges.sh
->>>>>>> 2cf02b9d6bb1bf5f58d6ef52614e56f95f14ee3c
 sudo rm -rf /home/ec2-user/server/processor/server_prestart.sh
 sudo rm -rf /home/ec2-user/server/processor/server_clear.sh
--- scripts/server_prestart.sh
+++ scripts/server_prestart.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/processor
-<<<<<<< HEAD
 sudo sh server_start.sh
-=======
-sudo sh server_start.sh
->>>>>>> 2cf02b9d6bb1bf5f58d6ef52614e56f95f14ee3c
+
